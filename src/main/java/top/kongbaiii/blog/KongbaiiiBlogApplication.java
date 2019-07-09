021b091c765e01c1ed53189271484e0b08fb1ed9
kongbaiii <yj.ns97@gmail.com>
2019-07-09 14:44:54 +0800

    first commit project init

---
+++ src/main/java/top/kongbaiii/blog/KongbaiiiBlogApplication.java
@@ -0,0 +1,17 @@
+package top.kongbaiii.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author kongbaiii
+ * @create 2019-07-09
+ */
+@SpringBootApplication
+public class KongbaiiiBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KongbaiiiBlogApplication.class, args);
+    }
+
+}
---
+++ src/main/java/top/kongbaiii/blog/entity/User.java
@@ -0,0 +1,14 @@
+package top.kongbaiii.blog.entity;
+
+import javax.persistence.Entity;
+
+/**
+ * @author kongbaiii
+ * @create 2019-07-09 13:15
+ */
+@Entity
+public class User {
+
+
+
+}
